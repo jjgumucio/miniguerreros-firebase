8c158927d390c1eb0d97ba50e8e0606e1b0ef3aa
Joaquín Gumucio L <jjgumucio@gmail.com>
2021-06-16 09:35:51 -0400

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,143 @@
+<script>
+  import { FirebaseApp, User, Doc, Collection } from "sveltefire";
+
+  import firebase from "firebase/app";
+  import "firebase/firestore";
+  import "firebase/auth";
+  import "firebase/performance";
+  import "firebase/analytics";
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyALJlVM6FipkEcK6mxuoDalf73Y9WEkCPU",
+    authDomain: "web-miniguerreros.firebaseapp.com",
+    projectId: "web-miniguerreros",
+    storageBucket: "web-miniguerreros.appspot.com",
+    messagingSenderId: "380447793338",
+    appId: "1:380447793338:web:79995a07638a70aea1a283",
+    measurementId: "G-DXZFCZ4TC9",
+  };
+
+  firebase.initializeApp(firebaseConfig);
+</script>
+
+<main>
+  {#if !firebaseConfig.projectId}
+    <strong>Step 0</strong>
+    Create a
+    <a href="https://firebase.google.com/">Firebase Project</a>
+    and paste your web config into
+    <code>App.svelte</code>
+    .
+  {/if}
+
+  <!-- 1. 🔥 Firebase App -->
+  <FirebaseApp {firebase}>
+    <h1>💪🔥 Mode Activated</h1>
+
+    <p>
+      <strong>Tip:</strong>
+      Open the browser console for development logging.
+    </p>
+
+    <!-- 2. 😀 Get the current user -->
+    <User let:user let:auth>
+      Howdy 😀! User
+      <em>{user.uid}</em>
+
+      <button on:click={() => auth.signOut()}>Sign Out</button>
+
+      <div slot="signed-out">
+        <button on:click={() => auth.signInAnonymously()}> Sign In Anonymously </button>
+      </div>
+
+      <hr />
+
+      <!-- 3. 📜 Get a Firestore document owned by a user -->
+      <Doc path={`posts/${user.uid}`} let:data={post} let:ref={postRef} log>
+        <h2>{post.title}</h2>
+
+        <p>
+          Document created at <em>{new Date(post.createdAt).toLocaleString()}</em>
+        </p>
+
+        <span slot="loading">Loading post...</span>
+        <span slot="fallback">
+          <button
+            on:click={() =>
+              postRef.set({
+                title: "📜 I like Svelte",
+                createdAt: Date.now(),
+              })}
+          >
+            Create Document
+          </button>
+        </span>
+
+        <!-- 4. 💬 Get all the comments in its subcollection -->
+
+        <h3>Comments</h3>
+        <Collection
+          path={postRef.collection("comments")}
+          query={(ref) => ref.orderBy("createdAt")}
+          let:data={comments}
+          let:ref={commentsRef}
+          log
+        >
+          {#if !comments.length}
+            No comments yet...
+          {/if}
+
+          {#each comments as comment}
+            <p>
+              <!-- ID: <em>{comment.ref.id}</em> -->
+            </p>
+            <p>
+              {comment.text}
+              <button on:click={() => comment.ref.delete()}>Delete</button>
+            </p>
+          {/each}
+
+          <button
+            on:click={() =>
+              commentsRef.add({
+                text: "💬 Me too!",
+                createdAt: Date.now(),
+              })}
+          >
+            Add Comment
+          </button>
+
+          <span slot="loading">Loading comments...</span>
+        </Collection>
+      </Doc>
+    </User>
+  </FirebaseApp>
+</main>
+
+<!-- Styles -->
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1,
+  em {
+    color: #ff3e00;
+  }
+
+  hr {
+    height: 1px;
+    border: none;
+    background: rgb(195, 195, 195);
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
